a20eda15ac484959c24e8a7bcd70c1b0bfe41f7b
Anish Sheela <aneesh.nl@gmail.com>
2015-10-15 05:06:01 +0530

    Implement check availability and domain suggestion

--- src/domain/Domain.php
+++ src/domain/Domain.php
@@ -4,19 +4,50 @@ require_once __DIR__ . '/../core/Core.php';
 
 class Domain extends Core {
   public function checkAvailability($domainName, $tlds, $suggestAlternatives = FALSE) {
-    
+    $avail = array(
+      'domain-name' => $domainName,
+      'tlds' => $tlds,
+      'suggest-alternative' => $suggestAlternatives
+    );
+    $apiOut = $this->callApi('domains', 'available', $avail);
+    return $apiOut;
   }
   
   public function checkAvailabilityIdn($domainName, $tld, $idnLanguageCode) {
-    
+    $punyDomain = array();
+    if(is_array($domainName)) {
+      foreach($domainName as $domain) {
+        // Convert domain to puny code so that it can be
+        // handled in ASCII itself
+        $punyDomain[] = idn_to_ascii($domain);
+      }
+    } else {
+      $punyDomain[] = idn_to_ascii($domainName);
+    }
+    $avail = array(
+      'domain-name' => $punyDomain,
+      'tlds' => $tlds,
+      'suggest-alternative' => $suggestAlternatives
+    );
+    $apiOut = $this->callApi('domains', 'idn-available', $avail);
+    return $apiOut;
   }
   
   public function checkAvailabilityPremium($keyWord, $tlds, $options) {
-    
+    // TODO: Rewrite using new API call
+    $options['key-word'] = $keyWord;
+    $options['tlds'] = $tlds;
+    $apiOut = $this->callApi('domains', 'premium-available', $avail);
+    return $apiOut;
   }
   
   public function domainSuggestions($keyWord, $tld = NULL, $exactMatch = FALSE) {
-    
+    // TODO: Rewrite using new API call
+    $options['key-word'] = $keyWord;
+    $options['tld-only'] = $tld;
+    $options['exact-match'] = $exactMatch;
+    $apiOut = $this->callApi('domains', 'suggest-names', $avail);
+    return $apiOut;
   }
   
   public function register($domainName, $tld, $options) {
