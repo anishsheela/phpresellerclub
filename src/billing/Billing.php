9a5dec57cb7d9026758eb2ec70962c1e688e8470
Anish Sheela <aneesh.nl@gmail.com>
2015-12-03 07:17:42 +0530

    Partly complete Billing Module

--- src/billing/Billing.php
+++ src/billing/Billing.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../core/Core.php';
 
 class Billing extends Core {
+
   public function getCustomerPricing($customerId) {
 
   }
@@ -91,23 +92,58 @@ class Billing extends Core {
 
   }
 
-  public function getAllowedPaymentGatewayCustomer($customerId = NULL, $paymentType) {
-    // handle both API calls
+  /**
+   * Get allowed payment gateway for a customer
+   * 
+   * @param int $customerId Customer ID
+   * @param string $paymentType Values can be AddFund or Payment.
+   * @return array Parsed output of API call
+   */
+  public function getAllowedPaymentGatewayCustomer($customerId, $paymentType = NULL) {
+    $options['customer-id'] = $customerId;
+    if (!empty($paymentType)) {
+      $options['payment-type'] = $paymentType;
+    }
+    $apiOut = $this->callApi('pg', 'allowedlist-for-customer', $options);
+    return $apiOut;
   }
 
+  /**
+   * Get allowed Payment Gateways
+   * @return array Parsed output of API call
+   */
   public function getAllowedPaymentGatewayReseller() {
-
+    $apiOut = $this->callApi('pg', 'list-for-reseller', array());
+    return $apiOut;
   }
 
   public function getCurrencyDetails() {
-
+    $apiOut = $this->callApi('currency', 'details', array());
+    return $apiOut;
   }
 
+  /**
+   * Get list of country
+   *
+   * @see http://manage.resellerclub.com/kb/answer/1746
+   * @return array Parsed output of API call
+   */
   public function getCountryList() {
-
+    $apiOut = $this->callApi('country', 'list', array());
+    return $apiOut;
   }
 
+  /**
+   * Get list of states of a given country
+   *
+   * @see http://manage.resellerclub.com/kb/answer/1747
+   * @param string $countryCode 2 letter country code
+   * @return array Parsed output of API call
+   */
   public function getStateList($countryCode) {
-    
+    $apiOut = $this->callApi('country', 'state-list', array(
+      'country-code' => $countryCode,
+    ));
+    return $apiOut;
   }
 }
