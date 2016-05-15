84b39189239d770b41639c81807c1cfe61d0a28b
Anish Sheela <aneesh.nl@gmail.com>
2016-05-15 23:16:36 +0530

    Add tests to validate function

--- Tests/src/validation/ValidationTest.php
+++ Tests/src/validation/ValidationTest.php
@@ -1,5 +1,8 @@
 <?php
 
+// TODO: use autoloader
+require_once __DIR__ . '/../../../src/index.php';
+
 /**
  * Created by PhpStorm.
  * User: anish
@@ -8,6 +11,34 @@
  */
 class ValidationTest extends PHPUnit_Framework_TestCase {
 
-  public function testGetValidationFunction() {
+  /**
+   * @var Validation
+   */
+  protected $object;
+
+  /**
+   * Sets up the fixture, for example, opens a network connection.
+   * This method is called before a test is executed.
+   */
+  protected function setUp() {
+    $mock = $this->getMock('Validation', array('callApi'));
+    $mock->method('callApi')->willReturn('foo');
+    $this->object = $mock;
+  }
+
+  /**
+   * Tears down the fixture, for example, closes a network connection.
+   * This method is called after a test is executed.
+   */
+  protected function tearDown() {
+
+  }
+
+  /**
+   * @covers Validation::validate
+   */
+  public function testValidateEmail() {
+    $this->assertTrue($this->object->validate('string', 'email', 'mail@example.com'));
+    $this->assertFalse($this->object->validate('string', 'email', 'mail@example'));
   }
 }
