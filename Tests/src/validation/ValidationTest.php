caff3003f59bc0c07f7e3c3b0344c822c86df001
Anish Sheela <aneesh.nl@gmail.com>
2016-06-13 21:28:02 +0530

    Clear validations

--- Tests/src/validation/ValidationTest.php
+++ Tests/src/validation/ValidationTest.php
@@ -2,15 +2,8 @@
 
 namespace Resellerclub;
 
-// TODO: use autoloader
 require_once __DIR__ . '/../../../src/index.php';
 
-/**
- * Created by PhpStorm.
- * User: anish
- * Date: 12/5/16
- * Time: 12:29 AM
- */
 class ValidationTest extends \PHPUnit_Framework_TestCase {
 
   /**
